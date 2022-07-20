8cffd6bed3f455b474fe512454da59044f96d4cf
Rezaofc6 <108604209+Rezaofc6@users.noreply.github.com>
2022-07-20 11:40:02 +0700

    Update info-donasi.js

--- plugins/info-donasi.js
+++ plugins/info-donasi.js
@@ -4,11 +4,12 @@ let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
 let pepe = pp ? await (await fetch(pp)).buffer() : Buffer.alloc(0)
 let str = ` ${conn.getName(m.sender)} Want Support Bot?
 *PAYMENT ↓*
-_*Pulsa/Pulse(Telkomsel):*_ 082287219167
-_*Dana/Gopay:*_ 0895612153565
+_*Pulsa/Pulse(3):*_ 089606790112
+_*Dana/Gopay:*_ 089606790112
+_*follow Ig :*_ https://instagram.com/itz.reza_official_
 Setelah melakukan donasi kirim bukti pembayaran ke owner,Dengan melakukan donasi,kamu telah mensupport bot ini agar on 24 jam dan fast respon
 `
-conn.sendHydrated(m.chat, str, wm, pepe, 'https://github.com/FahriAdison/', '𝙶𝙸𝚃𝙷𝚄𝙱', null, null, [
+conn.sendHydrated(m.chat, str, wm, pepe, 'https://chat.whatsapp.com/BCgUSCxQltq8SwcpJe1tDf', 'GROUP BOT', null, null, [
 ['𝙼𝙴𝙽𝚄', '/menu']
 ], m)
 
